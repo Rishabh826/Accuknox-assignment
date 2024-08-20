008467db3fb97d6081ee5c3dab4bc76c17df9feb
Rishabh826 <rishabhjangid0707@gmail.com>
2024-08-20 17:49:32 +0530

    colour change

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -5,7 +5,7 @@ import WidgetContext from "../Context/WidgetContext";
 const Content = () => {
   const { fetchedData } = useContext(WidgetContext);
   return (
-    <div className="bg-blue-50 py-5 px-10">
+    <div className="bg-grey-50 py-5 px-10">
       <Headers />
       {fetchedData.categories.map((category) => (
         <Category key={category.id} title={category.title} id={category.id} />
